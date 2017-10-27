ab8160fc316525ad739a789740096a3e0cd00a98
Mergim <MergimAlija@gmx.de>
2017-10-27 18:01:16 +0200

    test

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ var SOCKET_LIST = {};
 var io = require ('socket.io')(serv,{});
 io.sockets.on('connection',function(socket){
 	socket.id = Math.random();
-	socket.x = 0;
-	socket.y = 0;
-	socket.number = ""+Math.floor(10*Math.random());
-
 	SOCKET_LIST[socket.id] = socket;
 
 	socket.on('disconnect', function(){
@@ -32,9 +28,15 @@ io.sockets.on('connection',function(socket){
 	socket.on('sendMsgToServer',function(data){
 		for(var i in SOCKET_LIST){
 			SOCKET_LIST[i].emit('addToChat', data);
-		}
-
+			}
 	});
+
+	socket.on('door', function(data){
+        console.log('zelle:');
+        console.log(data);
+
+    });
+
 });
 
 setInterval(function(){
